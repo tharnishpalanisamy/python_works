8a6986800c5bbe82e7b5e7c5fff73e7150b824ef
Tharnish palanisamy <tharnishpalanisamy3@gmail.com>
2025-04-18 13:45:54 +0530

    Update snake.py

--- Mini_projects/snake_game/snake.py
+++ Mini_projects/snake_game/snake.py
@@ -11,11 +11,12 @@ class Snake:
         self.blocks = []
         self.create_snake()
         self.head = self.blocks[0]
-
+        self.can_turn = True
+        
     def create_snake(self):
         for position in STARTING_POSITION:
             self.add_blocks(position)
-
+            
     def add_blocks(self , position):
         new_block = Turtle(shape="square")
         new_block.penup()
@@ -25,26 +26,35 @@ class Snake:
 
     def extend(self):
         self.add_blocks(self.blocks[-1].position())
-      
+
     def move(self):
         for i in range(len(self.blocks) - 1, 0, -1):
             x_value = self.blocks[i - 1].xcor()
             y_value = self.blocks[i - 1].ycor()
             self.blocks[i].goto(x=x_value, y=y_value)
         self.head.forward(20)
-      
+        self.can_turn = True
+
     def up(self):
-        if self.head.heading() != DOWN:
+        if self.head.heading() != DOWN and self.can_turn:
             self.head.setheading(UP)
+            self.can_turn = False
+
     def down(self):
-        if self.head.heading() != UP:
+        if self.head.heading() != UP and self.can_turn:
             self.head.setheading(DOWN)
+            self.can_turn = False
+
     def left(self):
-        if self.head.heading() != RIGHT:
+        if self.head.heading() != RIGHT and self.can_turn:
             self.head.setheading(LEFT)
+            self.can_turn = False
+
     def right(self):
-        if self.head.heading() != LEFT:
+        if self.head.heading() != LEFT and self.can_turn:
             self.head.setheading(RIGHT)
+            self.can_turn = False
+
 
 
 
